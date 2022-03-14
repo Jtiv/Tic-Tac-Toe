2fcdc3a1ac43d5037e8c897db12d7d3b3bc236da
James Thompson <jamesthompson@Jamess-Mac-mini-4.local>
2022-03-14 15:58:01 -0400

    completed most game logic and recreated interactions. Just need to finish win condition, and score UI

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 let squares = document.querySelectorAll(".square");
 let turn = 0;
+let blueArr = [];
+let redArr = [];
+let winArr = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [1,4,7],
+    [2,5,8],
+    [3,6,9],
+    [1,5,9],
+    [3,5,7]
+]
+
+    
 
 let handleClick = (e) =>{
     turn++
-    e.target.classList.add("blue");
+        if(turn%2===0){
+            e.target.classList.add("blue");
+            blueArr.push(parseInt(e.target.innerHTML));
+        }else{
+             e.target.classList.add("red");
+            redArr.push(parseInt(e.target.innerHTML));
+        }
     console.log("clicked!", turn);
+    e.target.removeEventListener("click", handleClick);
 }
 
 
-
-for(let i=0; i<squares.length; i++){
+for(let i=0; i < squares.length; i++){
     squares[i].addEventListener("click", handleClick);
 }
